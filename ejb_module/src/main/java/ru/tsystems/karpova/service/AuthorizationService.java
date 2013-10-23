79613f44304807620392ed8c4dbaddf292fd5d91
KarpovaEkaterina <ekaterina.karpova.92@gmail.com>
2013-10-23 22:27:01 +0000

    modules reintroduced

--- ejb_module/src/main/java/ru/tsystems/karpova/service/AuthorizationService.java
+++ ejb_module/src/main/java/ru/tsystems/karpova/service/AuthorizationService.java
@@ -1,5 +1,6 @@
 package ru.tsystems.karpova.service;
 
+import org.apache.log4j.Logger;
 import ru.tsystems.karpova.dao.UserDAO;
 import ru.tsystems.karpova.entities.User;
 
@@ -9,22 +10,22 @@ import javax.ejb.Stateless;
 @Stateless
 public class AuthorizationService {
 
-    //private static Logger log = Logger.getLogger(AuthorizationService.class);
+    private final static Logger log = Logger.getLogger(AuthorizationService.class);
 
     @EJB
     UserDAO userDAO;
 
     public int login(String login, String password) {
-        //log.debug("Start \"login\" method");
-        //log.info(login + " is trying to authorize");
+        log.debug("Start \"login\" method");
+        log.info(login + " is trying to authorize");
         User user = userDAO.loadUserByLogin(login);
         if (user == null || user.getPassword() == null || !user.getPassword().equals(password)) {
-            //log.info("Authorization error.");
-            //log.debug("Send AuthorizationRespondInfo to client with WRONG_CREDENTIALS_STATUS");
+            log.info("Authorization error.");
+            log.debug("Send AuthorizationRespondInfo to client with WRONG_CREDENTIALS_STATUS");
             return 0;
         }
-        //log.info("Auth passed");
-        //log.debug("Send AuthorizationRespondInfo to client with OK_STATUS");
+        log.info("Auth passed");
+        log.debug("Send AuthorizationRespondInfo to client with OK_STATUS");
         return user.getAccessLevel();
     }
 }
